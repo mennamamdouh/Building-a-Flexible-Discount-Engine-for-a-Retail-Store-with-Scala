3319d0c65da833be3f558dd2672f157857506e92
mennamamdouh <es-menna.mamdouh1701@alexu.edu.eg>
2024-05-05 04:49:41 +0300

    added the Orders table creation code built in Oracle to store final results

---
+++ OrdersTable.sql
@@ -0,0 +1,10 @@
+CREATE TABLE ORDERS (
+    OrderTime VARCHAR2(50),
+    ProductName VARCHAR2(255),
+    ExpiryDate VARCHAR2(50),
+    Quantity INT,
+    UnitPrice DECIMAL(10, 2),
+    Channel VARCHAR2(20),
+    PaymentMethod VARCHAR2(20),
+    FinalPrice DECIMAL(10, 2)
+)
